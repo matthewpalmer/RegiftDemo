3b2ce35a5d684af2ba869ae0487b86794ffd84b0
matthewpalmer <matt@matthewpalmer.net>
2014-12-28 14:12:42 +1100

    Initial commit and set up

--- RegiftDemo/ViewController.swift
+++ RegiftDemo/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
+import MobileCoreServices
+import Regift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var finishedSelecting: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if !finishedSelecting {
+            presentVideoPicker()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        // Toggle state
+        finishedSelecting = true
+        
+        // Dismiss the picker
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        if let url = info[UIImagePickerControllerMediaURL] as? NSURL {
+            createGifFromURL(url)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Toggle state
+        finishedSelecting = true
 
+        // Dismiss the picker
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    // MARK: Gif creation
+    func createGifFromURL(URL: NSURL) {
+        let gifURL = Regift.createGIFFromURL(URL, withFrameCount: 16, delayTime: 0.2)
+        
+        println("gif URL is \(gifURL)")
+    }
+    
+    // MARK: Picker presentation
+    func presentVideoPicker() {
+        // Set up our video picker
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        
+        // Taken on the device's camera
+        picker.sourceType = UIImagePickerControllerSourceType.Camera
+        
+        // Videos only
+        picker.mediaTypes = [kUTTypeMovie]
+        
+        // Present the picker
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
 }
-
